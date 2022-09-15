0df78d3d82d070eaf39063724bd619ae90186db2
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-09-15 11:13:30 +0200

    Add project files.

---
+++ KyhStringDemo/App.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyhStringDemo
+{
+    internal class App
+    {
+        public void Run()
+        {
+            string name = "Stefan";
+            int age = 12;
+            Console.WriteLine("Du heter " + name + " och är " + age + " år gammal");
+            Console.WriteLine($"Du heter {name} och är {age} år gammal men om tre år är dy {age+3}");
+            string email = "stefan.hol@aaa.se"; // stefan@aaa.se
+            //Kontrollera så att det finns minst 2 . i email
+            
+            int numberOfDotsIHaveFoundSoFar = 0;
+            foreach (char letter in email)
+            {
+                if (letter == '.')
+                    numberOfDotsIHaveFoundSoFar++;
+            }
+            Console.WriteLine("Det finns " + numberOfDotsIHaveFoundSoFar + " punkter");
+
+            //string aName = "Stefan";
+            //char firstLetter = aName[0];
+            //char nextLetter = aName[1];
+            //for (int i = 0; i < aName.Length; i++)
+            //{
+            //    Console.WriteLine(aName[i]);
+            //}
+
+            //foreach (char letter in aName)
+            //{
+            //    Console.WriteLine(letter);
+            //}
+
+
+            //string namnet = Console.ReadLine();
+
+
+            //if (namnet.EndsWith(".se"))
+            //{
+            //    Console.WriteLine("Nice");
+            //}
+
+            ////namnet = namnet.TrimStart();//Tar bort mellanslag i början men inte efter
+            ////namnet = namnet.TrimEnd();//Tar bort mellanslag i början men inte efter
+            //namnet = namnet.Trim();
+            ////stefan  
+
+            //if (namnet.Length > 5)
+            //{
+            //    Console.WriteLine("Dui har skrivit för långt");
+            //}
+
+            ////text.LastIndexOf()
+            ////                  
+            //string name = "c:\\temp\\blabla\\hello.jpg";
+            //int pos = name.LastIndexOf("\\");
+            //string fileName = name.Substring(pos+1);
+            //Console.WriteLine(pos);
+
+            //string n2 = "hej hopp stefan";
+            //string n3 = n2.Substring(4, 3);
+            //Console.WriteLine(n3);
+
+            //Längden
+            //Trim
+            //StartsWith
+            //EndsWith
+            // Substring
+            // delar av sträng med []
+            // splitting
+            // looping
+
+
+
+
+//            Console.WriteLine("Välkommen till chatten");
+//            while (true)
+//            {
+//                Console.WriteLine("Säg nåt:");
+//                string text = Console.ReadLine();
+                
+//                // string lowerText = text.ToLower();
+//                text = text.Replace("dumma", "*****");
+//                text = text.Replace("dummis", "*****");
+                
+//                //if (lowerText.Contains("dumma"))
+//                //{
+//                //    Console.WriteLine("You are banned from the chat");
+//                //    break;
+//                //}
+//                //int position = lowerText.IndexOf("dumma"); // -1
+//                //if (position != -1)
+//                //{
+//                //    Console.WriteLine("You are banned from the chat");
+//                //    break;
+//                //}
+
+
+
+//                Console.WriteLine(text);
+//            }
+
+//            while (true)
+//            {
+//                Console.WriteLine("Spelet körs");
+//                Console.WriteLine("Vill du fortsätta Ja/Nej");
+//                string action = Console.ReadLine();
+                
+//                action = action.ToUpper();
+
+//                if (action=="NEJ")
+//                {
+//                    break;
+//                }
+//            }
+//            Console.WriteLine("Spelet är slut");
+
+
+//            int age = 12;
+//            int age2 = 0;
+//            age = age + 13;
+////          age 25
+
+//            string namn = "S";
+//            string namn1 = "sdghasdasdasdasdajhsdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdad";
+//            namn = "sdghasdasdasdasdajhsdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdad";
+//            namn1 = "A";
+
+
+//            string fornamn = "Stefan";
+//            string efternamn = "Holmberg";
+//            string fullName = fornamn + efternamn;
+//            Console.WriteLine(fullName);
+
+//            string tal1= "25";
+//            string tal2= "2";
+//            string summa= tal1 + tal2;
+//            Console.WriteLine(summa);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ KyhStringDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using KyhStringDemo;
+
+var app = new App();
+app.Run();
+
